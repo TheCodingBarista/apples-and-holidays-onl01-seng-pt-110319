cd662537d8033d2d45340aac765ae1fe7ff48a4c
Melissa Holt <melissa.holt21@gmail.com>
2019-11-19 23:36:08 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -27,7 +27,8 @@ end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
-new_holiday_hash 
+new_holiday_hash = []
+
 end
 
 def all_supplies_in_holidays(holiday_hash)
