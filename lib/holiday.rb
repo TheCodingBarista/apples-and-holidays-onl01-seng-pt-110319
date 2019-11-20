5e6b20a396d43e6814757769265cd52bfff36c29
Melissa Holt <melissa.holt21@gmail.com>
2019-11-20 00:27:43 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -37,9 +37,8 @@ end
 def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday|
     puts "#{season.capitalize}:"
-  holiday.each do |second_holiday, item|
-    
-    puts "  #{second_holiday.to_s.split{"_"}.map{|a| a.capitalize}.join(" ")}: #{item.join(", ")}"
+    holiday.each do |second_holiday, item|
+      puts "  #{second_holiday.to_s.split("_").map {|i| i.capitalize}.join(" ")}: #{item.join(", ")}"
     end
   end
 end
