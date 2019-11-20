dd946a9216386eb979b219e41a2ce9857e89aebc
Melissa Holt <melissa.holt21@gmail.com>
2019-11-20 00:18:26 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -43,11 +43,11 @@ def all_supplies_in_holidays(holiday_hash)
   end
 end
 
-#def all_holidays_with_bbq(holiday_hash)
+def all_holidays_with_bbq(holiday_hash)
 
   holiday_hash.map do |season, holiday|
     holiday.map do |holiday, item|
      holiday if item.include?("BBQ")
-    end.flatten.compact
-  end
+    end
+  end.flatten.compact
 end
